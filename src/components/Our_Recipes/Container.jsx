fc8fc40f32becea9bd7a22ebca9c996ddf942ac0
R0F7 <wwwrafikhan075@gmail.com>
2024-03-17 00:14:21 +0600

    change description for  slider & our recipes

--- src/components/Our_Recipes/Container.jsx
+++ src/components/Our_Recipes/Container.jsx
@@ -37,7 +37,7 @@ const Container = () => {
         <div >
             <div className="text-center">
                 <h1 className="text-[#150B2B] text-[40px] font-semibold mb-6">Our Recipes</h1>
-                <p className="text-[rgba(21,11,43,0.60)] text-base font-normal w-[55%] mx-auto mb-12">Lorem ipsum dolor sit amet consectetur. Proin et feugiat senectus vulputate netus pharetra rhoncus. Eget urna volutpat curabitur elementum mauris aenean neque. </p>
+                <p className="text-[rgba(21,11,43,0.60)] text-base font-normal w-[55%] mx-auto mb-12">Discover mouthwatering recipes for every craving. From savory delights to sweet treats, explore our diverse collection of culinary creations. </p>
             </div>
 
             <div className="flex mb-[100px]">
--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -6,9 +6,9 @@ const Slider = () => {
         <div className="slider-container flex flex-col justify-center items-center p-[170px] rounded-3xl text-center">
             <h1 className="text-[#FFF] text-[52px] font-bold mb-6">Discover an exceptional cooking <br />
             class tailored for you!</h1>
-            <p className="text-[#FFF] text-[18px] font-normal mb-10 w-3/4">Learn and Master Basic Programming, Data Structures, Algorithm, OOP, Database and solve 500+ coding problems to become an exceptionally well world-class Programmer.</p>
+            <p className="text-[#FFF] text-[18px] font-normal mb-10 w-3/4">Explore bespoke culinary courses designed just for you! Unveil the art of cooking with our exclusive classes, curated to elevate your skills and delight your taste buds.</p>
             <div className="flex gap-x-6">
-                <button className="text-[#150B2B] text-xl font-semibold bg-[#0BE58A] px-[30px] py-[20px] rounded-[50px]">Explore Now</button>
+                <button className="text-[#150B2B] text-xl font-semibold bg-[#35f3a3] px-[30px] py-[20px] rounded-[50px]">Explore Now</button>
                 <button className="text-[#FFF] text-xl font-semibold bg-transparent border border-[#FFF] px-[30px] py-[20px] rounded-[50px]">Our Feedback</button>
             </div>
         </div>
